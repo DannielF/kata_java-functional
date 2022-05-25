1d2fca019f2cd52d664b464f6289601d79694a82
dannielf <danielgranados1992@gmail.com>
2022-05-25 11:03:15 -0500

    [Add] kata 3 done

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -1,11 +1,11 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -13,9 +13,15 @@ import java.util.List;
     Output: List of Integers
 */
 public class Kata3 {
+
+    private Kata3() {
+    }
+
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        return movieLists.stream()
+                .flatMap(movieList -> movieList.getVideos().stream().map(Movie::getId))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/katas/Kata3Test.java
+++ src/test/java/katas/Kata3Test.java
@@ -1,7 +1,13 @@
 package katas;
 
+import model.Movie;
+import model.MovieList;
 import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -10,6 +16,13 @@ public class Kata3Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata3.execute().size(), equalTo(4));
+
+        List<MovieList> movieLists = DataUtil.getMovieLists();
+
+        var moviesIdOnly = movieLists.stream()
+                .flatMap(movieList -> movieList.getVideos().stream().map(Movie::getId))
+                .collect(Collectors.toList());
+
+        Assert.assertThat(moviesIdOnly.size(), equalTo(4));
     }
 }

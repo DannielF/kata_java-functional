58a229711066f448fe47ad4a579063b2f3575011
dannielf <danielgranados1992@gmail.com>
2022-05-25 10:34:30 -0500

    [Add] kata 2 done

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -1,12 +1,10 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -14,9 +12,14 @@ import java.util.Map;
     Output: List of Integers
 */
 public class Kata2 {
+
+    private Kata2() {
+    }
+
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+        return movies.stream().map(ele -> ele.getRating().intValue())
+                .filter(rating -> rating == 5.0).collect(Collectors.toList());
     }
 }
--- src/test/java/katas/Kata2Test.java
+++ src/test/java/katas/Kata2Test.java
@@ -1,15 +1,32 @@
 package katas;
 
-import org.junit.Assert;
+import model.Movie;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata2Test {
 
+
+
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata2.execute().size(), equalTo(4));
+
+        List<Movie> movies = DataUtil.getMovies();
+
+        var moviesRatingGthanFive = movies.stream().map(ele -> ele.getRating().intValue())
+                .filter(rating -> rating == 5.0).collect(Collectors.toList());
+
+        assertThat(moviesRatingGthanFive.get(0), equalTo(5));
+
     }
 }

d7e749906d9b2816ae0ca37c8e0a7cdbeab59165
dannielf <danielgranados1992@gmail.com>
2022-05-25 20:01:02 -0500

    [Add] kata 8 done

--- src/main/java/katas/Kata8.java
+++ src/main/java/katas/Kata8.java
@@ -1,11 +1,8 @@
 package katas;
 
 import com.codepoetics.protonpack.StreamUtils;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
 import model.Movie;
-import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
@@ -17,12 +14,19 @@ import java.util.Map;
     Output: List of ImmutableMap.of("videoId", "5", "bookmarkId", "3")
 */
 public class Kata8 {
-    public static List<Map> execute() {
+
+    private Kata8() {
+    }
+
+    public static List<Map<String, Integer>> execute() {
         List<Movie> movies = DataUtil.getMovies();
         List<Bookmark> bookMarks = DataUtil.getBookMarks();
 
         // StreamUtils.zip()
 
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        return StreamUtils.zip
+                        (movies.stream(), bookMarks.stream(),
+                                (a, b) -> Map.of("videoId", a.getId(), "bookMarkId", b.getId()))
+                .toList();
     }
 }
--- src/test/java/katas/Kata8Test.java
+++ src/test/java/katas/Kata8Test.java
@@ -1,15 +1,31 @@
 package katas;
 
-import org.junit.Assert;
+import com.codepoetics.protonpack.StreamUtils;
+import model.Bookmark;
+import model.Movie;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata8Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata8.execute(), equalTo(4));
+
+        List<Movie> movies = DataUtil.getMovies();
+        List<Bookmark> bookMarks = DataUtil.getBookMarks();
+
+        var zipped = StreamUtils.zip
+                        (movies.stream(), bookMarks.stream(),
+                                (a, b) -> Map.of("videoId", a.getId(), "bookMarkId", b.getId()))
+                .toList();
+
+        assertThat(zipped.size(), equalTo(3));
     }
 }

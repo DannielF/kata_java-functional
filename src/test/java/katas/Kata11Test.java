028ceee973364ae291c7c2ddb845f5422f9bfc7a
dannielf <danielgranados1992@gmail.com>
2022-05-25 22:43:45 -0500

    [Add] kata 11 done

--- src/test/java/katas/Kata11Test.java
+++ src/test/java/katas/Kata11Test.java
@@ -1,15 +1,45 @@
 package katas;
 
-import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata11Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata11.execute(), equalTo(4));
+
+        List<Map> lists = DataUtil.getLists();
+        List<Map> videos = DataUtil.getVideos();
+        List<Map> boxArts = DataUtil.getBoxArts();
+        List<Map> bookmarkList = DataUtil.getBookmarkList();
+
+        var newList = lists.stream()
+                .map(list -> Map.of("name", list.get("name"),
+                        "videos", videos
+                                .stream()
+                                .filter(video -> video.get("listId")
+                                        .equals(list.get("id")))
+                                .map(video -> Map.of("id", video.get("id"), "title", video.get("title"),
+                                        "time", bookmarkList
+                                                .stream()
+                                                .filter(bookM -> bookM.get("videoId")
+                                                        .equals(video.get("id")))
+                                                .map(book -> book.get("time")).toList(),
+                                        "boxArt", boxArts.stream()
+                                                .filter(boxA -> boxA.get("videoId")
+                                                        .equals(video.get("id")))
+                                                .min(Comparator.comparingInt(box -> (int) box.get("width")))))
+                                .toList()))
+                .toList();
+
+        assertThat(newList.size(), equalTo(2));
     }
 }
